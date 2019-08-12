6bb47cd94a4681e926f9e64ebf6e426277977494
BanuPriyaNaidu91 <41759248+BanuPriyaNaidu91@users.noreply.github.com>
2019-08-12 14:40:29 +0530

    Update plot5.R

--- plot5.R
+++ plot5.R
@@ -8,22 +8,27 @@ download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_d
 unzip(zipfile = "dataFiles.zip")
 
 # Load the NEI & SCC data frames.
-NEI <- data.table::as.data.table(x = readRDS("summarySCC_PM25.rds"))
-SCC <- data.table::as.data.table(x = readRDS("Source_Classification_Code.rds"))
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
 # Gather the subset of the NEI data which corresponds to vehicles
-vehiclesSCC <- SCC[grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
-                   , SCC]
-vehiclesNEI <- NEI[NEI[, SCC] %in% vehiclesSCC,]
+vehicles <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
+vehiclesSCC <- SCC[vehicles,]$SCC
+vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
 
 # Subset the vehicles NEI data to Baltimore's fip
-baltimoreVehiclesNEI <- vehiclesNEI[fips=="24510",]
+baltimoreVehiclesNEI <- vehiclesNEI[vehiclesNEI$fips=="24510",]
 
-png("plot5.png")
+png("plot5.png",width=480,height=480,units="px",bg="transparent")
 
-ggplot(baltimoreVehiclesNEI,aes(factor(year),Emissions)) +
-  geom_bar(stat="identity", fill ="#FF9999" ,width=0.75) +
+library(ggplot2)
+
+ggp <- ggplot(baltimoreVehiclesNEI,aes(factor(year),Emissions)) +
+  geom_bar(stat="identity",fill="grey",width=0.75) +
+  theme_bw() +  guides(fill=FALSE) +
   labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
-  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999 to 2008"))
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
+
+print(ggp)
 
 dev.off()
